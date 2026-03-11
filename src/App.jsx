abac91049e61fd50219674afd82ebabe7eb97155
4i6 <joaopcaetanoc@gmail.com>
2026-03-11 15:10:39 -0300

    Started the Chatbot page and started learning React features

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,48 @@ import './App.css'
 function App(props) {
   function ChatInput(){
     return (
-      <div>
-        <input />
+      <>
+        <input 
+        placeholder="Chat with the Chatbox here" size="30"
+        />
         <button>Send</button>
-      </div>
+      </>
     )
   }
+
+  function ChatMessage({message, sender}){
+      const senderConfig = {
+        chatbot: {
+          img: "../../utils/images/mikupog.png",
+          alt: "Bot Avatar"
+        },
+        user: {
+          img: "../../utils/images/sparxiepog.png",
+          alt: "user Avatar"
+        }
+      }
+
+      const config = senderConfig[sender] || senderConfig.user;
+      return(
+        <div>
+          {message}
+          <img src ={config.img} width="50"/>
+        </div>
+      );
+    }
+
   return (
     <div>
-      <h1>{props.titulo}</h1>
-      <ChatInput /> {/* Component syntax, our own HTML element */}
+      <ChatInput /> 
+      {/* Component syntax, our own HTML element */}
+      <ChatMessage 
+        message="hello chatbot" 
+        sender="user"
+      />
+      <ChatMessage 
+        message="hi there!" 
+        sender="chatbot"
+      />
     </div>
   );
 }
